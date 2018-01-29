ed7c1ca096ffccf719dafdf509e802819405bd20
prabeshghimire321 <prabeshghimire321@gmail.com>
2018-01-29 21:42:32 +0545

    delete.php added

--- delete.php
+++ delete.php
@@ -2,8 +2,8 @@
 require_once("dbconn.php");
 $id = $_GET["id"];
 $query = "DELETE FROM `participantsDetails` WHERE `participantsDetails`.`id` = '".$id."'";
-$result = $conn->query($query);
-if($result){
+$res = $conn->query($query);
+if($res){
   header("Location:/home.php");
 }else{
   echo "Not Deleted";
