ee2c7e5557749545e4ccaa11e705f3b9261968a8
prabeshghimire321 <prabeshghimire321@gmail.com>
2018-01-29 22:15:12 +0545

    head.php file updated

--- head.php
+++ head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <title>Bootcamp</title>
   <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="/bootcamp/phplogo.png" sizes="16x16">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
