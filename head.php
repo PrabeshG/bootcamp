05bac64adac7b5bf6407cd13128839db68b8eb86
prabeshghimire321 <prabeshghimire321@gmail.com>
2018-01-29 07:15:14 +0545

    head.php file added

---
+++ head.php
@@ -0,0 +1,9 @@
+<head>
+  <meta charset="utf-8">
+  <title>Bootcamp</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
