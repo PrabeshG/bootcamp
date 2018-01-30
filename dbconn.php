5f72a0ca4895de993dc33325c661eda055d13592
prabeshghimire321 <prabeshghimire321@gmail.com>
2018-01-30 15:38:18 +0545

    dbconn.php file modified

--- dbconn.php
+++ dbconn.php
@@ -1,8 +1,8 @@
 <?php
-
+session_start();
 $user = 'root';
 $password = '';
 $db = 'bootcampweb';
-$conn = new mysqli('localhost',$user,$password,$db) or die("DATABASE NOT CONNECTED");
+$conn = new mysqli('localhost',$user,$password,$db) or die("DATABASE CONNECTION FAILED TO START");
 
  ?>
