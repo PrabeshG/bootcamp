3332590b5a641fbc1e299502a38112f50a9bccc3
prabeshghimire321 <prabeshghimire321@gmail.com>
2018-02-04 08:56:06 +0545

    modified

--- createnew.php
+++ createnew.php
@@ -1,50 +1,49 @@
+<?php require_once("dbconn.php"); 
+
+?>
 <!DOCTYPE html>
 <html>
   <?php include("head.php") ?>
   <body>
-    <header style="background-color:#e9ecef;">
+    <header>
+      <nav class="navbar navbar-expand-sm fixed-top" style="background-color: #e9ecef;">
       <div class="container">
-        <div class="row">
-          <div class="col-sm-10">
-          <h2 style="color:#bbb;"><a href="/bootcamp/index.php" class="btn btn-outline-secondary " style="text-decoration: none;">
-            Home
-            <!-- <img src="/bootcamp/phplogo.png" alt="php" style="width:50px;"> -->
-          </a> Add New Participants To<small> BootCamp on Web Technologies...</small></h2>
-        </div>
-        <div class="col-sm-2">
-          <!-- <a href="/bootcamp/index.php" class="btn btn-link">Back</a> -->
-        </div>
-      </div>
-      </div>
+        <ul class="navbar-nav">
+           <a class='btn btn-outline-secondary disabled text-primary' style="margin:5px;"><?php
+            echo $_SESSION["username"] ?></a>
+            <h4 class="navbar-brand"><a href="/bootcamp/index.php"><i class="fas fa-home"></i></a>Add new Participants</h4>
+          <li class="nav-item">
+            <a href='/bootcamp/blog.php' class='btn btn-outline-secondary' style="margin:5px;">Blog</a>
+          </li>
+          <li class="nav-item">
+             <a href='/bootcamp/logout.php' class='btn btn-outline-secondary' style="margin:5px;">Logout</a>
+          </li>
+        </ul>
+    </nav>
+    <br/><br/><br/><br/>
     </header>
     <div class="container">
       <div class="row">
         <div class="col-sm-6">
           <form  action="/bootcamp/save.php" method="post">
             <div class="form-group">
-              <label>Name</label>
+              <label>NAME</label>
               <input type="text" class="form-control" name="name" placeholder="name">
             </div>
             <div class="form-group">
-              <label>Email</label>
+              <label>EMAIL</label>
               <input type="email" class="form-control" id="email" name="email" placeholder="email">
             </div>
             <div class="form-group">
-              <label>Contact Number</label>
+              <label>CONTACT NUMBER</label>
               <input type="text" class="form-control" name="contact" placeholder="contact number">
             </div>
             <div class="form-group">
-              <label>Address</label>
+              <label>ADDRESS</label>
               <input type="text" class="form-control" name="address" placeholder="address">
             </div>
             <div class="form-group">
-              <label>Comments</label>
-              <textarea class="form-control" rows="5" id="comments" name="comments" placeholder="comments"></textarea>
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="form-group">
-              <label for="Age">Age</label>
+              <label for="Age">AGE</label>
               <select class="form-control" name="age">
                 <?php for($age=15;$age<=35;$age++){
                   ?>
@@ -54,23 +53,29 @@
               </select>
             </div>
             <div class="form-group">
-              <label>Gender</label>
+              <label>GENDER</label>
               <br/>
               <label class="radio-inline"><input type="radio" name="gender" value="male" />Male</label>
               <label class="radio-inline"><input type="radio" name="gender" value="female" />Female</label>
               <label class="radio-inline"><input type="radio" name="gender" value="other" />Other</label>
             </div>
+            </div>
+          <div class="col-sm-6">
             <div class="form-group">
-              <label>Education</label>
+              <label>EDUCATION</label>
               <input type="text" class="form-control" name="education" placeholder="education">
             </div>
             <div class="form-group">
-              <label>Semester</label>
+              <label>SEMESTER</label>
               <input type="text" class="form-control" name="semester" placeholder="semester">
             </div>
             <div class="form-group">
-              <label>College</label>
+              <label>COLLEGE</label>
               <input type="text" class="form-control" name="college" placeholder="college">
+            </div>
+            <div class="form-group">
+              <label>COMMENTS</label>
+              <textarea class="form-control" rows="5" id="comments" name="comments" placeholder="comments"></textarea>
             </div>
             <button type="submit" class="btn btn-outline-secondary" name="submit">Create</button>
             <button type="reset" class="btn btn-outline-danger" name="reset">Reset</button>
